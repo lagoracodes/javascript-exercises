db04f56b778963cf81b6b862f937370fd26cf2bc
lagoracodes <slivkalaura@gmail.com>
2021-05-09 01:14:14 +0200

    1 finished, 1 started

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  let result = '';
+  // if
 
-}
+  for (let i = 0; i < times; i++) {
+    result += word;
+  }
+  return result;
+};
 
-module.exports = repeatString
+module.exports = repeatString;
--- repeatString/repeatString.spec.js
+++ repeatString/repeatString.spec.js
@@ -1,19 +1,19 @@
-const repeatString = require('./repeatString')
+const repeatString = require('./repeatString');
 
-describe('repeatString', function() {
-  it('repeats the string', function() {
+describe('repeatString', function () {
+  it('repeats the string', function () {
     expect(repeatString('hey', 3)).toEqual('heyheyhey');
   });
-  xit('repeats the string many times', function() {
+  xit('repeats the string many times', function () {
     expect(repeatString('hey', 10)).toEqual('heyheyheyheyheyheyheyheyheyhey');
   });
-  xit('repeats the string 1 times', function() {
+  xit('repeats the string 1 times', function () {
     expect(repeatString('hey', 1)).toEqual('hey');
   });
-  xit('repeats the string 0 times', function() {
+  xit('repeats the string 0 times', function () {
     expect(repeatString('hey', 0)).toEqual('');
   });
-  xit('returns ERROR with negative numbers', function() {
+  xit('returns ERROR with negative numbers', function () {
     expect(repeatString('hey', -1)).toEqual('ERROR');
   });
   xit('repeats the string a random amount of times', function () {
@@ -22,16 +22,18 @@ describe('repeatString', function() {
     equals a number between 0 to 999 (this number will change everytime you run
     the test).*/
 
-    // DO NOT use Math.floor(Math.random() * 1000) in your code, 
+    // DO NOT use Math.floor(Math.random() * 1000) in your code,
     // this test generates a random number, then passes it into your code with a function parameter.
     // If this doesn't make sense, you should go read about functions here: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/fundamentals-part-3
-    const number = Math.floor(Math.random() * 1000)
+    const number = Math.floor(Math.random() * 1000);
     /*The .match(/((hey))/g).length is a regex that will count the number of heys
     in the result, which if your function works correctly will equal the number that
     was randomaly generated. */
-    expect(repeatString('hey', number).match(/((hey))/g).length).toEqual(number);
+    expect(repeatString('hey', number).match(/((hey))/g).length).toEqual(
+      number
+    );
   });
-  xit('works with blank strings', function() {
+  xit('works with blank strings', function () {
     expect(repeatString('', 10)).toEqual('');
   });
 });
