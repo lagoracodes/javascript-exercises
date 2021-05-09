fdef1c544ff3375350b4b2ce4cd2d75f6648885a
lagoracodes <slivkalaura@gmail.com>
2021-05-09 17:09:57 +0200

    update

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,11 +1,14 @@
 const repeatString = function (word, times) {
   let result = '';
-  // if
 
-  for (let i = 0; i < times; i++) {
-    result += word;
+  if (times < 0) {
+    return 'ERROR';
+  } else {
+    for (let i = 0; i < times; i++) {
+      result += word;
+    }
+    return result;
   }
-  return result;
 };
 
 module.exports = repeatString;
